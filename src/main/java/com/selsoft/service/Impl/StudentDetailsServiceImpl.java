44ce8eec702cbff491de4677df1c4206f52bf7dd
BHAVANKUMARS <bhavan01032000@gmail.com>
2024-08-26 22:45:08 +0530

    service name changed

--- src/main/java/com/selsoft/service/Impl/StudentDetailsServiceImpl.java
+++ src/main/java/com/selsoft/service/Impl/StudentDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class dd implements StudentDetailsService {
+public class StudentDetailsServiceImpl implements StudentDetailsService {
 
     @Autowired
     private StudentDetailsRepo studentDetailsRepo;
